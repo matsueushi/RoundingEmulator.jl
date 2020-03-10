0268f99a20f7721cd903453ef1611eef5ea1ee95
matsueushi <matsueushi@gmail.com>
2020-03-10 11:00:09 -0400

    Underflow in mul_up, mul_down

--- src/RoundingEmulation.jl
+++ src/RoundingEmulation.jl
@@ -16,34 +16,49 @@ twosum(a, b) = abs(a) > abs(b) ? fast_twosum(a, b) : fast_twosum(b, a)
 # Product
 
 function twoprod(a, b)
-    # TODO fix underflow
     x = a * b
     x, fma(a, b, -x)
 end
 
 # Rounding
 
-for (op, ar) in zip((:add, :mul), (:sum, :prod))
-    @eval begin
-        # RoundUp
-        function $(Symbol(op, "_up"))(a, b)
-            x, y = $(Symbol("two", ar))(a, b)
+function add_up(a, b)
+    x, y = twosum(a, b)
+    # y > 0
+    isless(zero(y), y) ? nextfloat(x) : x 
+end
 
-            # `isless` is different from `<`
-            # julia> -0.0 < 0.0
-            # false
+function add_down(a, b)
+    x, y = twosum(a, b)
+    # y < 0
+    isless(y, zero(y)) ? prevfloat(x) : x
+end
 
-            # julia> isless(-0.0, 0.0)
-            # true
+function mul_up(a, b)
+    # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
+    x, y = twoprod(a, b)
+    # y > 0
+    if abs(x) >= 2.0^(-969)
+        isless(zero(y), y) ? nextfloat(x) : x
+    else
+        mult = 2.0^537
+        s, s2 = twoprod(a * mult, b * mult)
+        t = (x * mult) * mult
+        t < s || (t == s && s2 > 0) ? nextfloat(x) : x
+    end
+end
 
-            isless(zero(y), y) ? nextfloat(x) : x # y > 0
-        end
-
-        # RoundDown
-        function $(Symbol(op, "_down"))(a, b)
-            x, y = $(Symbol("two", ar))(a, b)
-            isless(y, zero(y)) ? prevfloat(x) : x # y < 0
-        end
+function mul_down(a, b)
+    # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
+    x, y = twoprod(a, b)
+    # y < 0
+    if abs(x) >= 2.0^(-969)
+        isless(y, zero(y)) ? prevfloat(x) : x
+    else
+        mult = 2.0^537
+        s, s2 = twoprod(a * mult, b * mult)
+        t = (x * mult) * mult 
+        t > s || (t == s && s2 < 0) ? prevfloat(x) : x
     end
 end
 
