959cad48387b8c6a4bffb92e3ac96c57ffdd4eaa
matsueushi <matsueushi@gmail.com>
2020-03-11 23:31:29 -0400

    Type annotations

--- src/RoundingEmulation.jl
+++ src/RoundingEmulation.jl
@@ -34,9 +34,9 @@ function twoprod(a, b)
 end
 
 # Rounding
-# Add
 
 for T in (Float32, Float64)
+    # Add
     @eval begin 
         function add_up(a::$T, b::$T)
             x, y = twosum(a, b)
@@ -46,72 +46,75 @@ for T in (Float32, Float64)
                 x == typemin($T) && isfinite(a) && isfinite(b) ? -floatmax($T) : x
             end
         end
-    end
-end
 
-function add_down(a, b)
-    x, y = twosum(a, b)
-    if isfinite(x)
-        if y < zero(y)
-            prevfloat(x)
-        else
-            # y = 0 -> x = a + b
-            # 1) x ≂̸ 0 => x
-            # 2) x = 0, a = -b ≂̸ 0 => -0.0
-            # 3) x = 0, a = b = 0
-            #    (a, b) = (0.0, 0.0) => 0.0
-            #    (a, b) = (-0.0, 0.0) => -0.0
-            #    (a, b) = (-0.0, -0.0) => -0.0
-            x == zero(x) && (signbit(a) || signbit(b)) ? -zero(x) : x
+        function add_down(a::$T, b::$T)
+            x, y = twosum(a, b)
+            if isfinite(x)
+                if y < zero($T)
+                    prevfloat(x)
+                else
+                    # y = 0 -> x = a + b
+                    # 1) x ≂̸ 0 => x
+                    # 2) x = 0, a = -b ≂̸ 0 => -0.0
+                    # 3) x = 0, a = b = 0
+                    #    (a, b) = (0.0, 0.0) => 0.0
+                    #    (a, b) = (-0.0, 0.0) => -0.0
+                    #    (a, b) = (-0.0, -0.0) => -0.0
+                    x == zero($T) && (signbit(a) || signbit(b)) ? -zero($T) : x
+                end
+            else
+                x == typemax($T) && isfinite(a) && isfinite(b) ? floatmax($T) : x
+            end
         end
-    else
-        x == typemax(x) && isfinite(a) && isfinite(b) ? floatmax(x) : x
     end
-end
 
-# Sub
-sub_up(a, b) = add_up(a, -b)
-sub_down(a, b) = add_down(a, -b)
 
-# Mul
+    # Sub
+    @eval sub_up(a::$T, b::$T) = add_up(a, -b)
+    @eval sub_down(a::$T, b::$T) = add_down(a, -b)
 
-# (minimal exponent) - (significand bits) + 2 * (significand bits) - 1 ?
-_mul_th1_exponent(T::Type) = (2 -exponent_max(T)) + (Base.significand_bits(T) + 2) - 1
-_mul_th1(T::Type) = ldexp(one(T), _mul_th1_exponent(T))
-_mul_th2_exponent(T::Type) = -1 + (exponent_max(T) + Base.significand_bits(T)) ÷ 2
-_mul_th2(T::Type) = ldexp(one(T), _mul_th2_exponent(T))
 
-function mul_up(a, b)
-    # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
-    x, y = twoprod(a, b)
-    if isfinite(x)
-        if abs(x) > _mul_th1(typeof(x)) # not zero(x): (a, b) = (-2.1634867667116802e-200, 1.6930929484402486e-119) fails
-            y > zero(y) ? nextfloat(x) : x
-        else
-            mult = _mul_th2(typeof(x))
-            s, s2 = twoprod(a * mult, b * mult)
-            t = (x * mult) * mult
-            t < s || (t == s && s2 > zero(s)) ? nextfloat(x) : x
+    # Mul
+    # (minimal exponent) - (significand bits) + 2 * (significand bits) - 1 ?
+    @eval _mul_th1_exponent(::Type{$T}) = $((2 -exponent_max(T)) + (Base.significand_bits(T) + 2) - 1)
+    @eval _mul_th1(::Type{$T}) = $(ldexp(one(T), _mul_th1_exponent(T)))
+    @eval _mul_th2_exponent(::Type{$T}) = $(-1 + (exponent_max(T) + Base.significand_bits(T)) ÷ 2)
+    @eval _mul_th2(::Type{$T}) = $(ldexp(one(T), _mul_th2_exponent(T)))
+
+    @eval begin
+        function mul_up(a::$T, b::$T)
+            # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
+            x, y = twoprod(a, b)
+            if isfinite(x)
+                if abs(x) > _mul_th1($T) # not zero(x): (a, b) = (-2.1634867667116802e-200, 1.6930929484402486e-119) fails
+                    y > zero($T) ? nextfloat(x) : x
+                else
+                    mult = _mul_th2($T)
+                    s, s2 = twoprod(a * mult, b * mult)
+                    t = (x * mult) * mult
+                    t < s || (t == s && s2 > zero($T)) ? nextfloat(x) : x
+                end
+            else
+                x == typemin($T) && isfinite(a) && isfinite(b) ? -floatmax($T) : x
+            end
         end
-    else
-        x == typemin(x) && isfinite(a) && isfinite(b) ? -floatmax(x) : x
-    end
-end
-
-function mul_down(a, b)
-    # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
-    x, y = twoprod(a, b)
-    if isfinite(x)
-        if abs(x) >  _mul_th1(typeof(x)) # not zero(x): (a, b) = (6.640350825165134e-116, -1.1053488936824272e-202) fails
-            y < zero(y) ? prevfloat(x) : x
-        else
-            mult = _mul_th2(typeof(x))
-            s, s2 = twoprod(a * mult, b * mult)
-            t = (x * mult) * mult 
-            t > s || (t == s && s2 < zero(s)) ? prevfloat(x) : x
+        
+        function mul_down(a::$T, b::$T)
+            # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
+            x, y = twoprod(a, b)
+            if isfinite(x)
+                if abs(x) >  _mul_th1($T) # not zero(x): (a, b) = (6.640350825165134e-116, -1.1053488936824272e-202) fails
+                    y < zero($T) ? prevfloat(x) : x
+                else
+                    mult = _mul_th2($T)
+                    s, s2 = twoprod(a * mult, b * mult)
+                    t = (x * mult) * mult 
+                    t > s || (t == s && s2 < zero($T)) ? prevfloat(x) : x
+                end
+            else
+                x == typemax($T) && isfinite(a) && isfinite(b) ? floatmax($T) : x
+            end
         end
-    else
-        x == typemax(x) && isfinite(a) && isfinite(b) ? floatmax(x) : x
     end
 end
 
