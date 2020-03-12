88c9701e8f832a4012b3b9ffb88f26e28b5f35ef
matsueushi <matsueushi@gmail.com>
2020-03-11 23:08:17 -0400

    Use type template

--- src/RoundingEmulation.jl
+++ src/RoundingEmulation.jl
@@ -1,10 +1,21 @@
 module RoundingEmulation
 
-using Base: IEEEFloat
-
 export twosum, add_up, add_down
 export sub_up, sub_down
 export twoprod, mul_up, mul_down
+
+using Base: IEEEFloat
+
+if VERSION >= v"1.4"
+    using Base: exponent_bias, exponent_max
+else
+    # The following definitions of exponent_bias and exopnent_max are taken from julia, base/float.jl. 
+    # License is MIT: https://julialang.org/license
+    for T in (Float32, Float64)
+        @eval exponent_bias(::Type{$T}) = $(Int(Base.exponent_one(T) >> Base.significand_bits(T)))
+        @eval exponent_max(::Type{$T}) = $(Int(Base.exponent_mask(T) >> Base.significand_bits(T)) - exponent_bias(T))
+    end
+end
 
 # Sum
 
@@ -24,12 +35,17 @@ end
 
 # Rounding
 # Add
-function add_up(a, b)
-    x, y = twosum(a, b)
-    if isfinite(x)
-        y > zero(y) ? nextfloat(x) : x
-    else
-        x == typemin(x) && isfinite(a) && isfinite(b) ? -floatmax(x) : x
+
+for T in (Float32, Float64)
+    @eval begin 
+        function add_up(a::$T, b::$T)
+            x, y = twosum(a, b)
+            if isfinite(x)
+                y > zero($T) ? nextfloat(x) : x
+            else
+                x == typemin($T) && isfinite(a) && isfinite(b) ? -floatmax($T) : x
+            end
+        end
     end
 end
 
@@ -58,17 +74,21 @@ sub_up(a, b) = add_up(a, -b)
 sub_down(a, b) = add_down(a, -b)
 
 # Mul
-const_mul_th1(x) = oftype(x, 2)^-969
-const_mul_th2(x) = oftype(x, 2)^537
+
+# (minimal exponent) - (significand bits) + 2 * (significand bits) - 1 ?
+_mul_th1_exponent(T::Type) = (2 -exponent_max(T)) + (Base.significand_bits(T) + 2) - 1
+_mul_th1(T::Type) = ldexp(one(T), _mul_th1_exponent(T))
+_mul_th2_exponent(T::Type) = -1 + (exponent_max(T) + Base.significand_bits(T)) ÷ 2
+_mul_th2(T::Type) = ldexp(one(T), _mul_th2_exponent(T))
 
 function mul_up(a, b)
     # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
     x, y = twoprod(a, b)
     if isfinite(x)
-        if abs(x) > const_mul_th1(x) # not zero(x): (a, b) = (-2.1634867667116802e-200, 1.6930929484402486e-119) fails
+        if abs(x) > _mul_th1(typeof(x)) # not zero(x): (a, b) = (-2.1634867667116802e-200, 1.6930929484402486e-119) fails
             y > zero(y) ? nextfloat(x) : x
         else
-            mult = const_mul_th2(x)
+            mult = _mul_th2(typeof(x))
             s, s2 = twoprod(a * mult, b * mult)
             t = (x * mult) * mult
             t < s || (t == s && s2 > zero(s)) ? nextfloat(x) : x
@@ -82,11 +102,10 @@ function mul_down(a, b)
     # http://verifiedby.me/adiary/pub/kashi/image/201406/nas2014.pdf
     x, y = twoprod(a, b)
     if isfinite(x)
-        th =  const_mul_th1(x) # not zero(x): (a, b) = (6.640350825165134e-116, -1.1053488936824272e-202) failss
-        if abs(x) > th
+        if abs(x) >  _mul_th1(typeof(x)) # not zero(x): (a, b) = (6.640350825165134e-116, -1.1053488936824272e-202) fails
             y < zero(y) ? prevfloat(x) : x
         else
-            mult = const_mul_th2(x)
+            mult = _mul_th2(typeof(x))
             s, s2 = twoprod(a * mult, b * mult)
             t = (x * mult) * mult 
             t > s || (t == s && s2 < zero(s)) ? prevfloat(x) : x
