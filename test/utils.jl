186c969d169b9e0426d71b0089da98a4b857545e
matsueushi <matsueushi@gmail.com>
2020-03-20 22:42:07 -0400

    Refactoring test (#4)
    * Combine check_op and check_op_sqrt
    
    * Adapt rounding_check_op to sqrt

--- test/utils.jl
+++ test/utils.jl
@@ -1,13 +1,14 @@
 import Base.Rounding
 using Printf
 
-function check_op(op, updown, ai, bi, calc, raw)
+function check_op(op, updown, calc, raw, args...)
     if isequal(calc, raw)
         true
     else
         @info("Erorr", op, updown)
-        @info(@sprintf("a = %0.18e, bit rep : %s", ai, bitstring(ai)))
-        @info(@sprintf("b = %0.18e, bit rep : %s", bi, bitstring(bi)))
+        for (i, v) in enumerate(args)
+            @info(@sprintf("a%d = %0.18e, bit rep : %s", i, v, bitstring(v)))
+        end
 
         @info(@sprintf("calc = %0.18e, bit rep : %s", calc, bitstring(calc)))
         @info(@sprintf("raw = %0.18e, bit rep : %s", raw, bitstring(raw)))
@@ -15,29 +16,28 @@ function check_op(op, updown, ai, bi, calc, raw)
     end
 end
 
-function rounding_check_op(op, base_op, a, b)
-    elt = eltype(a)
+function rounding_check_op(op, base_op, arrays...)
+    elt = eltype(arrays[1])
     @eval begin
         Rounding.setrounding_raw($elt, Rounding.to_fenv(RoundNearest))
-        up_calc = $(Symbol(op, "_up")).($a, $b)
-        down_calc = $(Symbol(op, "_down")).($a, $b)
+        up_calc = $(Symbol(op, "_up")).($(arrays...))
+        down_calc = $(Symbol(op, "_down")).($(arrays...))
 
         Rounding.setrounding_raw($elt, Rounding.to_fenv(RoundUp))
-        up_raw = broadcast($base_op, $a, $b)
+        up_raw = broadcast($base_op, $(arrays...))
 
         Rounding.setrounding_raw($elt, Rounding.to_fenv(RoundDown))
-        down_raw = broadcast($base_op, $a, $b)
+        down_raw = broadcast($base_op, $(arrays...))
 
         # Compare
-        for (ai, bi, calc, raw) in zip($a, $b, up_calc, up_raw)
-            @test check_op($op, "up", ai, bi, calc, raw)
+        for (calc, raw, args) in zip(up_calc, up_raw, $(arrays...))
+            @test check_op($op, "up", calc, raw, args)
         end
 
-        for (ai, bi, calc, raw) in zip($a, $b, down_calc, down_raw)
-            @test check_op($op, "down", ai, bi, calc, raw)
+        for (calc, raw, args) in zip(down_calc, down_raw, $(arrays...))
+            @test check_op($op, "down", calc, raw, args)
         end
     end
-
 end
 
 function rounding_check(a, b)
@@ -47,39 +47,7 @@ function rounding_check(a, b)
     Rounding.setrounding_raw(eltype(a), Rounding.to_fenv(RoundNearest))
 end
 
-function check_op_sqrt(op, updown, ai, calc, raw)
-    if isequal(calc, raw)
-        true
-    else
-        @info("Erorr", op, updown)
-        @info(@sprintf("a = %0.18e, bit rep : %s", ai, bitstring(ai)))
-
-        @info(@sprintf("calc = %0.18e, bit rep : %s", calc, bitstring(calc)))
-        @info(@sprintf("raw = %0.18e, bit rep : %s", raw, bitstring(raw)))
-        false
-    end
-end
-
 function rounding_check_sqrt(a)
-    elt = eltype(a)
-    Rounding.setrounding_raw(elt, Rounding.to_fenv(RoundNearest))
-    # Sqrt
-    up_calc = sqrt_up.(a)
-    down_calc = sqrt_down.(a)
-
-    Rounding.setrounding_raw(elt, Rounding.to_fenv(RoundUp))
-    up_raw = sqrt.(a)
-
-    Rounding.setrounding_raw(elt, Rounding.to_fenv(RoundDown))
-    down_raw = sqrt.(a)
-
-    # Compare
-    for (ai, calc, raw) in zip(a, up_calc, up_raw)
-        @test check_op_sqrt("sqrt", "up", ai, calc, raw)
-    end
-
-    for (ai, calc, raw) in zip(a, down_calc, down_raw)
-        @test check_op_sqrt("sqrt", "down", ai, calc, raw)
-    end
-    Rounding.setrounding_raw(elt, Rounding.to_fenv(RoundNearest))
+    rounding_check_op("sqrt", :sqrt, a)
+    Rounding.setrounding_raw(eltype(a), Rounding.to_fenv(RoundNearest)) 
 end
