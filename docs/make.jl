76ce93728292de11581b1fe6f17b0c1fbdec296d
matsueushi <matsueushi@gmail.com>
2020-03-23 23:19:37 -0400

    Documenter (#11)
    * link
    
    * Documenter

---
+++ docs/make.jl
@@ -0,0 +1,10 @@
+using Documenter, RoundingEmulator
+
+makedocs(
+    sitename = "RoundingEmulator.jl",
+    pages = [
+        "Home" => "index.md",
+        "Functions" => "functions.md",
+        "References" => "references.md"
+    ]
+)
